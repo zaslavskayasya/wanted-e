cf6e086cf1e3bfc25103d7d43ff378a41224a0df
Asya <web.zaslavskaya@gmail.com>
2023-01-23 23:06:29 +0200

    start menu, side-menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
 console.log(`Loaded main.js`);
+
+
+// header part
+
+let overflowDarken = document.querySelector('.overflow-block');
+let leftSideMenu = document.querySelector('#mySidenav');
+
+let openLeftSidebarMenu = document.querySelector('.openLeftSideMenu');
+let closeLeftSidebarMenu = document.querySelector('.closeLeftSidebar');
+
+
+openLeftSidebarMenu.addEventListener('click', ()=>{
+    leftSideMenu.style.left = "0px";
+    leftSideMenu.style.opacity = "1";
+    overflowDarken.style.opacity = "1";
+});
+
+closeLeftSidebarMenu.addEventListener('click', ()=>{
+    leftSideMenu.style.left = "-100%";
+    leftSideMenu.style.opacity = "0";
+    overflowDarken.style.opacity = "0";
+
+});
+
+
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
+
+
