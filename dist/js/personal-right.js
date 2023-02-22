c50f8b5c72865ca60c073bfa963f25b8da040029
Asya <web.zaslavskaya@gmail.com>
2023-02-22 19:31:46 +0200

    cabinet login / gegistration

---
+++ dist/js/personal-right.js
@@ -0,0 +1,45 @@
+console.log('personal test');
+
+let personalCabinetTrigger = document.querySelectorAll('.cabinet-trigger');
+let closeCabinetSidebar = document.querySelector('.closeCabinetSidebar');
+
+let cabinetSidebar = document.querySelector('.login-part');
+
+
+personalCabinetTrigger.forEach((triggerBtn)=>{
+  triggerBtn.addEventListener('click', (e)=>{
+    cabinetSidebar.classList.add('active');
+      overflowDarken.style.opacity = "1";
+      overflowDarken.style.pointerEvents  = "initial"; 
+      body.style.overflowY = "hidden"; 
+  });
+})
+
+
+closeCabinetSidebar.addEventListener('click', ()=>{
+    cabinetSidebar.classList.remove('active');
+    overflowDarken.style.opacity = "0";
+    overflowDarken.style.pointerEvents  = "none"; 
+     body.style.overflowY = "auto";
+});
+
+
+
+let triggerRegister = document.querySelector('.triggerRegister');
+let triggerLogin = document.querySelector('.triggerLogin');
+
+let registerblock = document.querySelector('.wrap-register');
+let loginBlock = document.querySelector('.wrap-login');
+
+
+
+triggerRegister.addEventListener('click', ()=>{
+    loginBlock.classList.remove('active');
+    registerblock.classList.add('active');
+});
+
+triggerLogin.addEventListener('click', ()=>{
+    registerblock.classList.remove('active');
+    loginBlock.classList.add('active');
+});
+//# sourceMappingURL=personal-right.js.map
